b7c3c9ab9bfa0a51897e3706eadb1cab5498b820
akshadachavann <chavanakshada59@gmail.com>
2025-01-21 00:11:26 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,38 @@
+# Single Line Comment
+
+''' 
+    Multiple 
+    Line
+    Comment
+'''
+
+# print("Hello World") # this is code for hello world 
+print("my name is Akshada")
+print("5+2")        # double iverted comma is shows output as it is.......................    function()
+#print("Manoj")
+#print("Mummy")
+#print("Pappa")
+#print("Didi")
+#print("Everyone")
+
+
+# simple numbers :integers(int e.g 1,2,3,4, etc.)
+# fractional numbers : float(float e.g 1.2,5.78,100.3692, etc. )
+
+''' Arithmetic operators ( + , - , * , / , // , % ) '''
+
+print(5+2) # addition
+print(5-2) # substraction
+print(5*2) # multiplication
+print(5//2) #floor division
+print(5/2) #simple division
+print(5%2) #modulus ( checking for reminder )
+
+a = "Sorriee  " 
+print(a*10)
+# while having space in between the strings
+# print(a)
+# print(a * 10)
+print("\nSorriee"*10)  # \n is used for new line
+print("Sorriee\t" * 10) # \t is used for tab space
+print("Sorriee")
